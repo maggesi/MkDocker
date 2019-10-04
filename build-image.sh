5103da14ed45c4c8ea5305aa21d23f23dd3a9c1d
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-10-04 23:11:45 +0200

    Update and rename travis-build-image.sh to build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,3 @@
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t hol-light-environment .
 docker tag hol-light-environment "$DOCKER_USERNAME"/hol-light-environment
 docker push "$DOCKER_USERNAME"/hol-light-environment
