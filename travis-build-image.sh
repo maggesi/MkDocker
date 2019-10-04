60c642c9420166915b14ded5cbefbf4f9379102c
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-10-04 18:47:06 +0200

    Simplify travis-build-image.sh

--- travis-build-image.sh
+++ travis-build-image.sh
@@ -1,5 +1,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t travis-ci-build-stages-demo .
+docker build -t hol-light-environment .
 docker images
-docker tag travis-ci-build-stages-demo $DOCKER_USERNAME/travis-ci-build-stages-demo
-docker push $DOCKER_USERNAME/travis-ci-build-stages-demo
+docker push hol-light-environment
