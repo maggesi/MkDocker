c28810bd08dea79ac3007e1374201ebbb7eaf665
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-10-04 19:13:53 +0200

    Fix docker push (?)

--- travis-build-image.sh
+++ travis-build-image.sh
@@ -1,4 +1,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t hol-light-environment .
-docker images
-docker push hol-light-environment
+docker tag hol-light-environment "$DOCKER_USERNAME"/hol-light-environment
+docker push "$DOCKER_USERNAME"/hol-light-environment
