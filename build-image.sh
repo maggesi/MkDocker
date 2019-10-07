f45df3f10f0476f605ec6aa3ff5955ec086160c9
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-10-07 15:06:56 +0200

    Revision
    Change name of the repository (hol-light).
    
    Use a build script for images.
    
    Use build phases "before_script" and deploy".

---
+++ build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Shell variables that need to be configured before running this script:
+# DOCKER_REPOSITOR
+# TOPDIR (directory containing images)
+# TAG    (subdirectory of images and tags of the images)
+
+set -ev
+IMAGE="$DOCKER_REPOSITORY:$TAG"
+cd $TOPDIR/images/$TAG
+docker pull "$IMAGE" || true
+docker build --cache-from "$IMAGE" -t $TAG .
+docker tag $TAG "$IMAGE"
