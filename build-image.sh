2c5f9fdf22bba0c3e83b8be8220dba2a2798a666
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-10-22 22:28:25 +0200

    Improve comments in build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Shell variables that need to be configured before running this script:
-# DOCKER_REPOSITOR
-# TOPDIR (directory containing images)
-# TAG    (subdirectory of images and tags of the images)
+# DOCKER_REPOSITOR: Name of the repository on Docker Hub
+# TOPDIR: folder containing the targets (folders containing Dockerfile)
+# TAG: the target (and docker tag), i.e., one of the subfolder of TOPDIR.
 
 set -ev
 IMAGE="$DOCKER_REPOSITORY:$TAG"
