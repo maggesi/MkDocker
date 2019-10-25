5555c20e10dc352e05abde8db612682db56dc2e5
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-10-25 19:51:46 +0200

    Fix test.

--- test_script.ml
+++ test_script.ml
@@ -1,7 +1,7 @@
 let FOO_THM = ARITH_RULE `b = 0 ==> (a + b) - a = 0`;;
 loads "update_database.ml";;
 report "DB thm names loadaded";;
-search[`x = 0 ==> (x + y) - x = 0`];;
+search[`y = 0 ==> (x + y) - x = 0`];;
 assert (length it >= 1);;
-report "Done!";;
-exit 0;;
+report "HOL Light test completed!";;
+
